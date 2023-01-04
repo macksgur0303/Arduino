fb7760069d878a6cccacf498c6694099d2c46e7a
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-01-04 16:53:10 +0900

    Add files via upload

---
+++ 2023.01.04/led_11.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+void setup()
+{
+  int i;
+  
+  for(i=2; i <=9; i++)
+  {
+    pinMode(2, OUTPUT);
+  }
+}
+
+void loop()
+{
+  int i;
+  
+  for(i=2; i<=9; i++)
+  {
+      digitalWrite(i, HIGH);
+      delay(500); // Wait for 1000 millisecond(s
+  }
+  
+  for(i=9; i<=2; i--)
+  {
+      digitalWrite(i, LOW);
+      delay(500); // Wait for 1000 millisecond(s
+  }
+
+  delay(500); // Wait for 1000 millisecond(s
+
+}
